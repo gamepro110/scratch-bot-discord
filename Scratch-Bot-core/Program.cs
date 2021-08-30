490b303812aa6ff94ab0d97cac96175979a0c142
void@Legion-5 <k.koelewijn2@gmail.com>
2021-08-30 21:16:37 +0200

    added missing file handling

--- Scratch-Bot-core/Program.cs
+++ Scratch-Bot-core/Program.cs
@@ -10,19 +10,26 @@ IApp app = (IApp)provider.GetService(typeof(IApp));
 
 if (app != null)
 {
-    string[] argLines = File.ReadAllLines(Settings.TokenFile);
-    if (argLines.Length == 2)
+    if (File.Exists(Settings.TokenFile))
     {
-        Settings.WebhookUrl = argLines[0]; // set the webhook url
-        CancellationTokenSource? tokenSource = provider.GetService<CancellationTokenSource>();
-        if (tokenSource != null)
+        string[] argLines = File.ReadAllLines(Settings.TokenFile);
+        if (argLines.Length == 2)
         {
-            await app.Run(argLines[1], tokenSource.Token);
+            Settings.WebhookUrl = argLines[0]; // set the webhook url
+            CancellationTokenSource? tokenSource = provider.GetService<CancellationTokenSource>();
+            if (tokenSource != null)
+            {
+                await app.Run(argLines[1], tokenSource.Token);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"something went wrong while reading {Settings.TokenFile}");
         }
     }
     else
     {
-        Console.WriteLine($"something went wrong while reading {Settings.TokenFile}");
+        Console.WriteLine($"{Settings.TokenFile} not found");
     }
 }
 else
